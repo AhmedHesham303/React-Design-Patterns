67659a3ddbb5a1e4afac831a952d5a3b92188b7b
AhmedHesham <ahmed.hesham303303@gmail.com>
2026-02-11 12:14:35 +0200

    commented styled components for now

--- React-Design-patterns/src/layoutComponents/SplitScreen.tsx
+++ React-Design-patterns/src/layoutComponents/SplitScreen.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const Container = styled.div`
-  display: flex;
-`;
+// const Container = styled.div`
+//   display: flex;
+// `;
 
 // const Panel = styled.div`
 //   flex: ${(p) => p.flex};
